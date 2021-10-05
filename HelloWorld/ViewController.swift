f0d677cba62a922814d979253b3ba9990410df4c
Xiong Ben <xiongben1993@gmail.com>
2021-10-05 21:03:06 +0800

    init

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -8,12 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var intNumber: Int = 0
+    var doubleNumber: Double = 0
+    var step: Int = 1
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        updateUI()
     }
 
 
+    @IBAction func clickButton(_ sender: UIButton) {
+        intNumber += step
+        doubleNumber += Double(step)
+        if intNumber >= 10 || intNumber <= 0 {
+            step *= -1
+        }
+        updateUI()
+        
+    }
+    
+    func updateUI() {
+        label.text = "Int: \(intNumber) \nDouble: \(doubleNumber)"
+        
+        
+        if step > 0 {
+            button.setTitle("+\(step)", for: .normal)
+        } else {
+            button.setTitle("\(step)", for: .normal)
+        }
+    }
 }
 
