894ca1f99f99b1067cc5263dc58df567240fd756
Xiong Ben <xiongben1993@gmail.com>
2021-10-06 23:38:00 +0800

    [add]

---
+++ HelloWorld/ListVC.swift
@@ -0,0 +1,29 @@
+//
+//  ListVC.swift
+//  HelloWorld
+//
+//  Created by Xiong Ben on 6/10/21.
+//
+
+import UIKit
+
+class ListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
