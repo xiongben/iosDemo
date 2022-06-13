3c239e5357fa813384ac5521488abc4e54800d4f
kakaxi <kakaxi@gmail.com>
2022-06-14 00:05:14 +0800

    [add]

--- LearnDemo/LearnDemo/Views/SimpleDemoView.swift
+++ LearnDemo/LearnDemo/Views/SimpleDemoView.swift
@@ -7,11 +7,26 @@
 
 import SwiftUI
 
+class DateViewModel:ObservableObject {
+    @Published var value:Int = 0
+    
+    init(){
+        for i in 1...10 {
+            DispatchQueue.main.asyncAfter(deadline: .now() + Double(i)) {
+                self.value += 1
+            }
+        }
+    }
+    
+}
+
 struct SimpleDemoView: View {
     @State private var count:Int = 0;
+    @StateObject var viewModel = DateViewModel()
     
     var body: some View {
         VStack {
+            Text("number = \(viewModel.value)")
             Text("click times: \(count)")
             Button("click button"){
                 count = count + 1
@@ -19,6 +34,8 @@ struct SimpleDemoView: View {
         }
     }
 }
+
+
 
 struct SimpleDemoView_Previews: PreviewProvider {
     static var previews: some View {
