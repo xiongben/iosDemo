2c7ee0362ec7efb0bebc54f3d343f0d0852e4e8d
kakaxi <kakaxi@gmail.com>
2022-06-15 21:24:58 +0800

    [add] badges demo

---
+++ LearnDemo/LearnDemo/Views/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,24 @@
+//
+//  RotatedBadgeSymbol.swift
+//  LearnDemo
+//
+//  Created by admin on 2022/6/15.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
