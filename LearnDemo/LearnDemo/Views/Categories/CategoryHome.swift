434485278f27f33411ac097c997ae0ea6ed31603
kakaxi <kakaxi@gmail.com>
2022-06-16 22:58:32 +0800

    [add]

---
+++ LearnDemo/LearnDemo/Views/Categories/CategoryHome.swift
@@ -0,0 +1,30 @@
+//
+//  CategoryHome.swift
+//  LearnDemo
+//
+//  Created by admin on 2022/6/16.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    @EnvironmentObject var modelData: ModelData
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
+                    Text(key)
+                }
+            }
+                .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+            .environmentObject(ModelData())
+    }
+}
